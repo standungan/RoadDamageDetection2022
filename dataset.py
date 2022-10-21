e826bd5453fbfe61cb01abb758b9da42954fef18
sofyan <standungan@gmail.com>
2022-10-21 23:56:32 +0800

    commit from gui

--- dataset.py
+++ dataset.py
@@ -1,49 +1,52 @@
 import torch
 import os
-import pandas as pd
-import torch
-import torchvision.transforms.functional as F
+import xml.etree.ElementTree as ET
+
 from PIL import Image
 from torch.utils.data import Dataset
-from utilities import transformer
-
-cat_name_to_id = {
-    "D00" : 1,
-    "D10" : 2,
-    "D20" : 3,
-    "D40" : 4,
-    "Repair" : 5,
-    }
+from utilities import transform
+labels_idx = ["D00","D10","D20","D40"]
 
 class RoadDamageDataset(Dataset):
-    
-    def __init__(self, dataset_name,split="TRAIN"):
-        self.dataset = pd.read_csv(dataset_name)    
-        self.filename = self.dataset.filename
+    def __init__(self, folder):
         
-        self.xmin = self.dataset.xmin
-        self.ymin = self.dataset.ymin
-        self.xmax = self.dataset.xmax
-        self.ymax = self.dataset.ymax
+        self.dir_xmls = os.path.join(folder, "annotations", "xmls")
+        self.dir_imgs = os.path.join(folder, "images")
         
-        self.labels = self.dataset.name
-        self.split = split
+        self.annotations = [os.path.join(self.dir_xmls, xml) for xml in os.listdir(self.dir_xmls)]
+        self.images_file = [os.path.join(self.dir_imgs, img) for img in os.listdir(self.dir_imgs)]
+
+    def __getitem__(self,i):
         
-    def __getitem__(self, i):
-        img = Image.open(self.filename[i])
-        img = img.convert('RGB')
+        image = Image.open(self.images_file[i])
+        tree = ET.parse(self.annotations[i])
+        objects = tree.findall("object")
+
+        bboxes = []
+        labels = []
         
-        bbox = torch.as_tensor([self.xmin[i], self.ymin[i], self.xmax[i], self.ymax[i]], 
-                               dtype=torch.float32)
+        for i, obj in enumerate(objects):
+            objectID = "object_{:02d}".format(i)
+            name = obj.find("name").text
+            if name == 'Repair':
+                continue
+            bbox = obj.find("bndbox")
+            xmin = int(bbox.find("xmin").text)
+            ymin = int(bbox.find("ymin").text)
+            xmax = int(bbox.find("xmax").text)
+            ymax = int(bbox.find("ymax").text)
+            
+            label = labels_idx.index(name)
+            labels.append(label)
+            bboxes.append([xmin, ymin, xmax, ymax])
         
-        label = torch.as_tensor(cat_name_to_id[self.labels[i]], 
-                                dtype=torch.float32)
+        bboxes = torch.FloatTensor(bboxes)
+        labels = torch.FloatTensor(labels)
         
-        img, bbox, label = transformer(img, bbox, label, self.split)
+        image, bboxes, labels = transform(image, bboxes, labels)
         
-        return img, bbox, label
-    
+        return image, bboxes, labels
+
     def __len__(self):
-        return len(self.dataset)
-    
-    
+        
+        return len(self.images_file)
